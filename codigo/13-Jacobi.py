4cdc8c86f2ac6643425120cc4f5c23d328a5e3b9
jnramirezg <jnramirez@unal.edu.co>
2021-10-11 23:26:17 -0500

    act

---
+++ codigo/13-Jacobi.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Método iterativo de Jacobi, construcción del método
+# %%Información
+'''
+Universidad Nacional de Colombia
+Facultad de Ingeniería y Arquitectura
+Métodos Numéricos Aplicados a la Ingeniería Civil
+
+Fecha de creación: 2021-10-11
+Autor: Juan Nicolás Ramírez Giraldo (jnramirezg@unal.edu.co)
+
+"Cum cogitaveris quot te antecedant, respice quot sequantur" Séneca
+
+1. Khoury R., Harder, D. W. (2016). Numerical Méthods and Modelling for 
+Engineering. Springer
+
+'''
+
+A = [
+     [ 3, -1, -1],
+     [-1,  3,  1],
+     [ 2,  1,  4],
+                 ]
+
+B = [1, 3, 7]
+
+# Matriz diagonal
+
+# Matriz triangular inferior
+
+# Matriz triangular superior
